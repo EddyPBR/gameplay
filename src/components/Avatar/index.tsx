86eef213f3ac7f67ad4969cd8fd0a415aa984a1b
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-01 15:35:04 -0300

    :package: Adicionando componente Avatar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { Image } from "react-native";
+
+import { theme } from "../../global/styles/theme";
+import { styles } from "./styles";
+
+type AvatarProps = {
+  urlImage: string;
+};
+
+export function Avatar({ urlImage }: AvatarProps) {
+  const { secondary50, secondary70 } = theme.colors;
+
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={[secondary50, secondary70]}
+    >
+      <Image source={{ uri: urlImage }} style={styles.avatar} />
+    </LinearGradient>
+  );
+}
