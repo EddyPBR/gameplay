d24cc733edd137d3ac28b2b42dbbd574670940fd
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-01 10:51:43 -0300

    :package: Criando component ButtonIcon

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import {
+  Text,
+  Image,
+  View,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+
+import DiscordImg from "../../assets/discord.png";
+import { styles } from "./styles";
+
+type ButtonIconProps = TouchableOpacityProps & {
+  title: string;
+};
+
+export function ButtonIcon({ title, ...rest }: ButtonIconProps) {
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+      <View style={styles.iconWrapper}>
+        <Image source={DiscordImg} style={styles.icon} />
+      </View>
+
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
