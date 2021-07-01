ae6133d37d1072c48c7883ccb1d684cfbaae59bc
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-01 14:01:39 -0300

    :package: Adicionando fontes, status bar no app

--- App.tsx
+++ App.tsx
@@ -1,6 +1,36 @@
 import React from "react";
+import { StatusBar } from "react-native";
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+import AppLoading from "expo-app-loading";
+
 import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
-  return <SignIn />;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <SignIn />
+    </>
+  );
 }
