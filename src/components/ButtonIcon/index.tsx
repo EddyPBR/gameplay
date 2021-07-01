eeab2c8ef842186fe8272ce9317cdaa14a18ad70
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-01 15:43:05 -0300

    :wrench: Alterando component touchableOpacity por RectButton no ButtonIcon component

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,28 +1,22 @@
 import React from "react";
-
-import {
-  Text,
-  Image,
-  View,
-  TouchableOpacity,
-  TouchableOpacityProps,
-} from "react-native";
+import { Text, Image, View } from "react-native";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 
 import DiscordImg from "../../assets/discord.png";
 import { styles } from "./styles";
 
-type ButtonIconProps = TouchableOpacityProps & {
+type ButtonIconProps = RectButtonProps & {
   title: string;
 };
 
 export function ButtonIcon({ title, ...rest }: ButtonIconProps) {
   return (
-    <TouchableOpacity style={styles.container} {...rest}>
+    <RectButton style={styles.container} {...rest}>
       <View style={styles.iconWrapper}>
         <Image source={DiscordImg} style={styles.icon} />
       </View>
 
       <Text style={styles.title}>{title}</Text>
-    </TouchableOpacity>
+    </RectButton>
   );
 }
