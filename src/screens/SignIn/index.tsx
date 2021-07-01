f1fa2e00886f129ffa638f23fecda58db6873414
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-01 09:50:06 -0300

    :octocat: Iniciando o projeto com expo init

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello signIn</Text>
+    </View>
+  );
+}
