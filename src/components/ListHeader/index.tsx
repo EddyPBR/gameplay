3ac3d389b1432d740e66e023700f32572c921025
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-01 20:27:21 -0300

    :package: Criando component ListHeader

---
+++ src/components/ListHeader/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { Avatar } from "../../components/Avatar";
+
+import { styles } from "./styles";
+
+type ListHeaderProps = {
+  title: string;
+  subtitle: string;
+};
+
+export function ListHeader({ title, subtitle }: ListHeaderProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.subtitle}>{subtitle}</Text>
+    </View>
+  );
+}
