3ede48f621cbb7edc387bb347b5edc344cd0ba6b
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-02 15:48:00 -0300

    :package: Adicionando rota para a tela de appointments

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import { Home } from "../screens/Home";
 import { SignIn } from "../screens/SignIn";
+import { AppointmentDetails } from "../screens/AppointmentDetails";
 
 import { theme } from "../global/styles/theme";
 
@@ -20,6 +21,7 @@ export function AuthRoutes() {
     >
       <Screen name="SignIn" component={SignIn} />
       <Screen name="Home" component={Home} />
+      <Screen name="AppointmentDetails" component={AppointmentDetails} />
     </Navigator>
   );
 }
