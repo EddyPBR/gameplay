73d29998ed8fdc61b8ea3b0842a83478e5bd4f97
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-06 19:44:21 -0300

    :package: Profile recebe dados do usuário autenticado

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import { useAuth } from "../../hooks/useAuth";
+
 import { Avatar } from "../../components/Avatar";
 
 import { styles } from "./styles";
 
 export function Profile() {
+  const { user } = useAuth();
+
   return (
     <View style={styles.container}>
-      <Avatar urlImage="https://github.com/eddypbr.png" />
+      <Avatar urlImage={user?.avatar} />
 
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá,</Text>
-          <Text style={styles.username}>Edvaldo</Text>
+          <Text style={styles.username}>{user?.firstName}</Text>
         </View>
 
         <Text style={styles.message}>Hoje é dia de vitória</Text>
