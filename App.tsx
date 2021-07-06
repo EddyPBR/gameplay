163afe7aa944fd817db4fa3d41507a9cca6b66dd
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-06 19:30:03 -0300

    :package: Adicionando AuthProvider na aplicação

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, LogBox } from "react-native";
 import { useFonts } from "expo-font";
 import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
 import {
@@ -8,7 +8,12 @@ import {
 } from "@expo-google-fonts/rajdhani";
 import AppLoading from "expo-app-loading";
 
+import { AuthProvider } from "./src/contexts/AuthContext";
+
 import { Routes } from "./src/routes";
+import { Background } from "./src/components/Background";
+
+// LogBox.ignoreLogs(["You are not currently signed in to Expo on your development machine"]);
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,13 +28,15 @@ export default function App() {
   }
 
   return (
-    <>
+    <Background>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      <Routes />
-    </>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </Background>
   );
 }
