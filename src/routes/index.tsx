27622437c4f0d0977445a2fee332e6e6a5ee23ce
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-06 19:36:31 -0300

    :package: Definindo roteamento para usuários autenticados

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
-import { Background } from "../../src/components/Background";
+import { useAuth } from "../hooks/useAuth";
 
+import { SignIn } from "../screens/SignIn";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
+  const { user } = useAuth();
+
   return (
-    <Background>
-      <NavigationContainer>
-        <AuthRoutes />
-      </NavigationContainer>
-    </Background>
+    <NavigationContainer>
+      {user.id ? <AuthRoutes /> : <SignIn />}
+    </NavigationContainer>
   );
 }
