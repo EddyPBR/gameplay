ce2aec2a1765e08b0406525c2d7f352fbe5153bd
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-08 14:49:57 -0300

    :package: Integrando página de appointment details com a API

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,40 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import { Text, ImageBackground, View, FlatList } from "react-native";
 import { Fontisto } from "@expo/vector-icons";
 import { BorderlessButton } from "react-native-gesture-handler";
+import { useRoute } from "@react-navigation/native";
+import { Alert } from "react-native";
 
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
 import { ListHeader } from "../../components/ListHeader";
-import { Member } from "../../components/Member";
+import { Member, MemberProps } from "../../components/Member";
 import { ListDivider } from "../../components/ListDivider";
 import { ButtonIcon } from "../../components/ButtonIcon";
+import { AppointmentProps } from "../../components/Appointment";
+import { Load } from "../../components/Load";
 
 import { theme } from "../../global/styles/theme";
 import { styles } from "./styles";
 import BannerImg from "../../assets/banner.png";
 
+import { api } from "../../services/api";
+
+type AppointmentDetailsParams = {
+  guildSelected: AppointmentProps;
+};
+
+type GuildWidget = {
+  id: string;
+  name: string;
+  instant_invite: string;
+  members: MemberProps[];
+  presence_count: number;
+};
+
 export function AppointmentDetails() {
-  const members = [
-    {
-      id: "1",
-      username: "Edvaldo",
-      avatar_url: "https://github.com/eddypbr.png",
-      status: "online",
-    },
-    {
-      id: "2",
-      username: "Edvaldo",
-      avatar_url: "https://github.com/eddypbr.png",
-      status: "offline",
-    },
-    {
-      id: "3",
-      username: "Edvaldo",
-      avatar_url: "https://github.com/eddypbr.png",
-      status: "online",
-    },
-  ];
+  const [widget, setWidget] = useState<GuildWidget>({} as GuildWidget);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const route = useRoute();
+  const { guildSelected } = route.params as AppointmentDetailsParams;
+
+  async function fetchGuildWidget() {
+    try {
+      const response = await api.get(
+        `/guilds/${guildSelected.guild.id}/widget.json`
+      );
+      setWidget(response.data);
+    } catch {
+      Alert.alert(
+        "Verifique as configurações do servidor. Será que o Widget esta habilitado?"
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchGuildWidget();
+  }, []);
+
   return (
     <Background>
       <Header
@@ -48,22 +72,29 @@ export function AppointmentDetails() {
 
       <ImageBackground source={BannerImg} style={styles.banner}>
         <View style={styles.bannerContent}>
-          <Text style={styles.title}>Lendários</Text>
-          <Text style={styles.subtitle}>
-            É hoje que vamos chegar ao challenger sem perder uma partida da md10
-          </Text>
+          <Text style={styles.title}>{guildSelected.guild.name}</Text>
+          <Text style={styles.subtitle}>{guildSelected.description}</Text>
         </View>
       </ImageBackground>
 
-      <ListHeader title="Jogadores" subtitle="Total 3" />
+      {isLoading ? (
+        <Load />
+      ) : (
+        <>
+          <ListHeader
+            title="Jogadores"
+            subtitle={`${widget?.members?.length}`}
+          />
 
-      <FlatList
-        data={members}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Member data={item} />}
-        ItemSeparatorComponent={() => <ListDivider isCentered />}
-        style={styles.members}
-      />
+          <FlatList
+            data={widget.members}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <Member data={item} />}
+            ItemSeparatorComponent={() => <ListDivider isCentered />}
+            style={styles.members}
+          />
+        </>
+      )}
 
       <View style={styles.footer}>
         <ButtonIcon title="Entrar na partida" />
