79206972d1fbcde307735f3bb48cede76864ee40
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-08 09:57:23 -0300

    :package: Tela de guilds resgata as guilds da API

--- src/screens/Guilds/index.tsx
+++ src/screens/Guilds/index.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, FlatList } from "react-native";
 
 import { Guild, GuildProps } from "../../components/Guild";
+import { Load } from "../../components/Load";
 import { ListDivider } from "../../components/ListDivider";
+
+import { api } from "../../services/api";
 
 import { styles } from "./styles";
 
@@ -11,95 +14,38 @@ type Props = {
 };
 
 export function Guilds({ handleGuildSelect }: Props) {
-  const guilds = [
-    {
-      id: "1",
-      name: "Lendários",
-      icon: "image.png",
-      owner: true,
-    },
-    {
-      id: "2",
-      name: "Lendários",
-      icon: "image.png",
-      owner: true,
-    },
-    {
-      id: "3",
-      name: "Lendários",
-      icon: "image.png",
-      owner: true,
-    },
-    {
-      id: "4",
-      name: "Lendários",
-      icon: "image.png",
-      owner: true,
-    },
-    {
-      id: "5",
-      name: "Lendários",
-      icon: "image.png",
-      owner: true,
-    },
-    {
-      id: "6",
-      name: "Lendários",
-      icon: "image.png",
-      owner: true,
-    },
-    {
-      id: "7",
-      name: "Lendários",
-      icon: "image.png",
-      owner: true,
-    },
-    {
-      id: "9",
-      name: "Lendários",
-      icon: "image.png",
-      owner: true,
-    },
-    {
-      id: "10",
-      name: "Lendários",
-      icon: "image.png",
-      owner: true,
-    },
-    {
-      id: "11",
-      name: "Lendários",
-      icon: "image.png",
-      owner: true,
-    },
-    {
-      id: "12",
-      name: "Lendários",
-      icon: "image.png",
-      owner: true,
-    },
-    {
-      id: "13",
-      name: "Lendários",
-      icon: "image.png",
-      owner: true,
-    },
-  ];
+  const [guilds, setGuilds] = useState<GuildProps[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  async function fetchGuilds() {
+    const response = await api.get("/users/@me/guilds");
+
+    setGuilds(response.data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    fetchGuilds();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={guilds}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Guild data={item} onPress={() => handleGuildSelect(item)} />
-        )}
-        showsVerticalScrollIndicator={false}
-        ItemSeparatorComponent={() => <ListDivider isCentered />}
-        ListHeaderComponent={() => <ListDivider isCentered />}
-        contentContainerStyle={{ paddingBottom: 68, paddingTop: 103 }}
-        style={styles.guilds}
-      />
+      {isLoading ? (
+        <Load />
+      ) : (
+        <FlatList
+          data={guilds}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Guild data={item} onPress={() => handleGuildSelect(item)} />
+          )}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <ListDivider isCentered />}
+          ListHeaderComponent={() => <ListDivider isCentered />}
+          contentContainerStyle={{ paddingBottom: 68, paddingTop: 103 }}
+          style={styles.guilds}
+        />
+      )}
     </View>
   );
 }
