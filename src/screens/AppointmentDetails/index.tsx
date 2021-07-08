61c700625e45ad56235e04dc37f5deea6dd847fb
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-08 08:53:46 -0300

    :wrench: Alterando estilo de appointment details

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -61,7 +61,7 @@ export function AppointmentDetails() {
         data={members}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <Member data={item} />}
-        ItemSeparatorComponent={() => <ListDivider />}
+        ItemSeparatorComponent={() => <ListDivider isCentered />}
         style={styles.members}
       />
 
